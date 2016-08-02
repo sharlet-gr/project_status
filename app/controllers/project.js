9945667fccd64f2b2177ae5549ef5b3bf96d41a9
sharlet-gr <swilson@grepruby.com>
2016-08-02 18:44:21 +0530

    Modify project

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -8,17 +8,14 @@ module.exports = (app, passport) => {
 	
 	//Admin posts a project
 	app.post('/projects', (req,res) => {
-		if(!req.user)
-			return res.json({"message":"You are not logged in"});
-		if(req.user.role!="Admin")
-			return res.json({"message":"You are not authorized to perform this action"});
+		
 		Project.findOne({$text: {$search: req.body.name}}, (err,project) => {
 			if(err)
 				return res.json(err);
 
 			//Do not save if the project already exists
 			if(project)
-				return res.json({"message":"This project already exists!"});
+				return res.json({"message":"This project name already exists!"});
 			
 			//Replace space after commas & then split at comma
 			req.body.clients = req.body.clients.replace(/,\s/g, ',').split(",");
@@ -35,30 +32,23 @@ module.exports = (app, passport) => {
 	app.get('/projects', (req,res) => {
 		if(!req.user)
 			return res.json({"message":"You are not logged in"});
-		if(req.user.role=="Employee")
-			return res.json({"message":"You are not authorized to perform this action"});
-		
-		//If user is a manager, only display the alloted projects
-		if(req.user.role=="Manager"){
-			Employee.findOne({_id:req.user._id}, (err,employee) => {
-				if(err)
-					return res.json(err);
-				employee.populate('projects',(err,employee) => {
-					if(err)
-						return err;
-					return res.json(employee.projects);
-				});
-			});
-		}
-		//If user is an admin, display all the projects
-		else
-		{
+		if(req.user.admin == true){
 			Project.find({},(err, projects) => {
 				if(err)
 					return res.json(err);
 				return res.json(projects);
 			});
 		}
+		//If user is not an admin, display only the alloted projects
+		Employee.findOne({_id:req.user._id}, (err,employee) => {
+			if(err)
+				return res.json(err);
+			employee.populate('projects',(err,employee) => {
+				if(err)
+					return err;
+				return res.json(employee.projects);
+			});
+		});
 	});
 
 	//Modify a project
@@ -66,81 +56,84 @@ module.exports = (app, passport) => {
 		Project.findOne({_id: req.params.projectId}, (err,project) => {
 			if(err)
 				return res.json(err);
-			if(!project)
-				return res.json({"message":"This project does not exist"});
 			
 			//If project name is modified
-			if(req.body.name!=undefined)
-				project.name = req.body.name;
-
-			//If clients are modified
+			if(req.body.name!=undefined && req.body.name!=project.name)
+			{
+				Project.findOne({$text: {$search: req.body.name}}, (err, project) => {
+					if(err)
+						return res.json(err);
+					if(project)
+						return res.json({"message":"This project name already exists!"});
+					project.name = req.body.name;
+				});
+	
+			}
+			//If clients are modified (list of clients is a comma separated list)
 			if(req.body.clients!=undefined){		
 				//Replace space after commas & then split at comma
 				req.body.clients = req.body.clients.replace(/,\s/g, ',').split(",");
-				project.clients = req.body.clients;
+				if(req.body.clients!=project.clients)
+					project.clients = req.body.clients;
 			}
-
-			//If managers are modified
-			if(req.body.managers!=undefined){
+			//If managers are modified (list of managers is an array)
+			if(req.body.members!=undefined){
+				//Remove project from employee model for managers that have been removed
 				
-				//Replace space after commas & then split at comma
-				req.body.managers = req.body.managers.replace(/,\s/g, ',').split(",");
-				
-				//To save the list of managers (oldManagers) that might have been removed in modification
-				let length = project.managers.length;
-				let oldManagers = [];
+				let length = project.members.length;
 				for(let i=0; i<length; i++){
-					if(!checkAvailability(req.body.managers, project.managers[i].toString())){
-						oldManagers.push(project.managers[i]);
+					if(!checkAvailability(req.body.members, project.members[i].person.toString())){
+						Employee.update({_id: project.members[i].person}, 
+							{$pull: {projects: project._id}}, (err) => {
+							if(err)
+								return(err);
+						});	
+						Project.update({_id: req.params.projectId},
+							{$pull: {members: {person: project.members[i].person}}}, 
+							(err) => {
+								if(err)
+									return(err);
+							});
 					}
 				}
-				project.managers = req.body.managers;
-				
-				//Save project in employee model for managers
-				length = project.managers.length;
+				length = req.body.members.length;
+				//Save project in project and employee model for managers
 				for(let i=0; i<length; i++){
-					Employee.update({_id: project.managers[i]},{$addToSet:{projects: project._id}}, (err) => {
+					let member = {person: req.body.members[i], role: req.body.role[i]};
+					Project.findByIdAndUpdate(req.params.projectId, 
+						{$addToSet: {members: member}}, (err, project) => {
+							if(err)
+								return(err);
+							for(let i=0; i<project.members.length; i++){
+								if(project.members[i].person == member.person && project.members[i].role != member.role){	
+									Project.findByIdAndUpdate(req.params.projectId, 
+										{$pull: {members: project.members[i]}},(err, project) => {
+											if(err)
+												res.json(err);
+									});
+									break;
+								}
+							}
+					});
+					Employee.findByIdAndUpdate(req.body.members[i],
+						{$addToSet: {projects: project._id}}, (err) => {
 						if(err)
 							return(err);
 					});
 				}
-
-				//Remove project from employee model for managers that have been removed
-				length = oldManagers.length;
-				for(let i=0; i<length; i++){
-					Employee.update({_id: oldManagers[i]}, {$pull: {projects: project._id}}, (err) => {
-						if(err)
-							return(err);
-					})
-				} 
-			}
-
-			//If employees are modified
-			if(req.body.employees!=undefined){	
-				//Replace space after commas & then split at comma
-				req.body.employees = req.body.employees.replace(/,\s/g, ',').split(",");
-				project.employees = req.body.employees;
 			}
 			project.save();
-			res.json(project);
+			res.json("Updation successful");	
 		});
 	});
 
 	//Delete a project
 	app.delete('/projects/:projectId', (req,res) => {
 		
-		//Check if the user is logged in & has the right to proceed
-		if(!req.user)
-			return res.json({"message":"You are not logged in"});
-		if(req.user.role!="Admin")
-			return res.json({"message":"You are not authorized to perform this action"});
-		
 		//Perform deletion only if user is an admin
 		Project.remove({_id: req.params.projectId}, (err, obj) => {
 			if(err)
 				return res.json(err);
-			if(obj.result.n === 0) 
-				return res.json({"message":"This project does not exist"});
 			
 			//Also remove the project from employee model
 			Employee.update({}, {$pull: {projects: req.params.projectId}}, {multi: true}, (err) => {
